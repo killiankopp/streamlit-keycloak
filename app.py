4d20459824d50867c2119fb952e12ac00ca84dfb
Killian KOPP <killiankopp@gmail.com>
2025-11-24 20:18:27 +0100

    Add initial Streamlit app with Keycloak authentication and .gitignore

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
-import streamlit as st
+from dataclasses import asdict
 from streamlit_keycloak import login
+import streamlit as st
 
+
+def main():
+    st.subheader(f"Welcome {keycloak.user_info['preferred_username']}!")
+    st.write(f"Here is your user information:")
+    st.write(asdict(keycloak))
+    if st.button("Disconnect"):
+        keycloak.authenticated = False
+
+
+st.title("Streamlit Keycloak example")
 keycloak = login(
     url="https://keycloak.amazone.lan",
     realm="koden",
     client_id="streamlit",
 )
 
-st.title("Bienvenue sur Streamlit")
-
-
-if not keycloak.authenticated:
-    st.warning("Utilisateur non authentifié")
-    st.stop()
-
-# Si authentifié :
-user_info = keycloak.user_info
-access_token = keycloak.access_token
-refresh_token = keycloak.refresh_token
-
-st.write("Connecté en tant que :", user_info["preferred_username"])
-st.write("Token d'accès :", access_token)
+if keycloak.authenticated:
+    main()
